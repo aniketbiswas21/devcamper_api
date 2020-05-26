78f596cb3cff98fc1be8bc96468b77a8804f82b4
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-05-26 22:58:36 +0530

    Added more functionalities

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-//Route Files
-const bootcamps = require("./routes/bootcamps");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
+//Route Files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+// Body Parser
+app.use(express.json());
+
+//Dev loggin middleware
+if (process.env.NODE_env === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `Server started on Port ${PORT} and in ${process.env.NODE_env} mode`
+    `Server started on Port ${PORT} and in ${process.env.NODE_env} mode`.yellow
+      .bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
