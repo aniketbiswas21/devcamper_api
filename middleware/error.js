cb54844cf36dd19ccca7e6a857ea80f32409bc7e
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-05-28 22:48:37 +0530

    More error handling and also added slugify

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,33 @@
+const ErrorResponse = require("../utils/errorResponse");
+
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
   //Log to console for dev
   console.log(err.stack.red);
 
-  res.status(err.statusCode || 500).json({
+  // Mongoose bad ObjectID
+  if (err.name === "CastError") {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message); //Object.values gets all the values from the array. Refer MDN docs for better explaination
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || "Server Error",
+    error: error.message || "Server Error",
   });
 };
 
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -96,6 +97,13 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create Bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  // console.log("Slugify Ran", this.name);
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
