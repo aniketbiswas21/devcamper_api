c9697068398ab2f03d6a980a9db91e54cebd32b8
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-05-29 13:25:07 +0530

    Added GeoJSON middleware

---
+++ utils/geocoder.js
@@ -0,0 +1,13 @@
+const NodeGeocoder = require("node-geocoder");
+
+// Refer node geocoder docs for more details
+const options = {
+  provider: process.env.GEOCODER_PROVIDER,
+  httpAdapter: "https",
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null,
+};
+
+const geocoder = NodeGeocoder(options);
+
+module.exports = geocoder;
