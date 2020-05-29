156f71d9ff934268f080e3859df80386d3623175
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-05-29 20:43:14 +0530

    Added more advanced mongo queries

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,8 +5,11 @@ const {
   deleteBootcamp,
   updateBootcamp,
   createBootcamp,
+  getBootcampsInRadius,
 } = require("../controllers/bootcamps");
 const router = express.Router();
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
 router.route("/").get(getBootcamps).post(createBootcamp);
 
